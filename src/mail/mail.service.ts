672abce2a0c70749542ce057eca00ca2eedc2e25
ZanaSiaka <146843697+ZanaSiaka@users.noreply.github.com>
2026-06-01 15:18:20 +0000

    push-up

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -36,4 +36,31 @@ export class MailService {
 
     }
 
+    async unlockMail(to: string, nom: string, token: string) {
+
+        const unlockUrl = `${process.env.FRONTEND_URL}/unlock-confirm?token=${token}`;
+
+        try {
+            const res = await this.resend.emails.send({
+                from: 'onboarding@resend.dev',
+                to: 'zanacoul210@gmail.com',
+                subject: 'Demande de déblocage de compte',
+                html: `
+                    <h2>Bonjour Mlle/Mme/M ${nom} ! </h2>
+                    <p>Vous avez demandé le déblocage de votre compte. Cliquez sur le lien ci-dessous pour confirmer :</p>
+                    <a href="${unlockUrl}" style="background-color: #4f46e5; color: white; padding: 12px 24px; text-decoration: none; display: inline-block; margin: 16px 0;"> Débloquer mon compte</a>
+                    <p>Ce lien expire dans 1 heure</p>
+                    <p>Si vous n'avez pas fait de demande, ignorez cet email</p>
+                `
+            });
+
+            if (res.error) throw new Error(res.error.message);
+            console.log('Email pour débloquage envoyé avec succès');
+        } catch (error) {
+            console.error("Error when email sending");
+            throw error;
+        }
+
+    }
+
 }
